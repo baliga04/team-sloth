26a274fb97dff724f98dea2afa2a1c635e607de4
Sayed Afnan Khazi <afnan@Sayeds-MacBook-Pro.local>
2023-05-27 17:06:32 +0530

    Revamp

--- backend/app/admin_endpoints/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Creating a Blueprint
-bp = Blueprint('admin_endpoints', __name__)
-
-# Importing the endpoints
-from app.admin_endpoints import endpoints
--- backend1/app.py
+++ backend1/app.py
@@ -1,13 +1,23 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+import os
+import re
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 
 
-app=Flask(__name__, template_folder='templates')
+load_dotenv()
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def p1():
-    return render_template('landingpage.html')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
-db={"":""}
+bcrypt = Bcrypt(app)
+# server_session=Session(app)
+db = SQLAlchemy(app)
 
 class Budgets(db.Model):
     UserID: int = db.Column(db.Integer, db.ForeignKey('user.UserID'), primary_key=True)
@@ -31,20 +41,67 @@ class User(db.Model):
     def __repr__(self):
          return f'<User>'
     
+class Transactions(db.Model):
+    TransactionID: int = db.Column(db.Integer, primary_key=True)
+    UserID: int = db.Column(db.Integer, db.ForeignKey('user.UserID'), nullable=False)
+    Amount: float = db.Column(db.Float, nullable=False)
+    Category: str = db.Column(db.String(50), nullable=False)
+    DateTime: str = db.Column(db.DateTime(timezone=True), nullable=False)
+    Description: str = db.Column(db.String(100), nullable=False)
+    
+    def __repr__(self):
+         return f'<Transactions>'
+    
+with app.app_context():
+    db.create_all()
+
+
+# Flask Bcrypt
+bcrypt = Bcrypt()
+
+# Validate Password
+
+
+def validate_password(password):
+
+    # Password checker
+    # Primary conditions for password validation:
+    # Minimum 8 characters.
+    # The alphabet must be between [a-z]
+    # At least one alphabet should be of Upper Case [A-Z]
+    # At least 1 number or digit between [0-9].
+    # At least 1 character from [ _ or @ or $ ]. 
+
+    #\s- Returns a match where the string contains a white space character
+    if len(password) < 8 or re.search("\s" , password):  
+        return False  
+    if not (re.search("[a-z]", password) and re.search("[A-Z]", password) and re.search("[0-9]", password) ):
+        return False  
+    return True  
+
+@app.route('/')
+def p1():
+    return render_template('landingpage.html')
+
+
+    
 @app.route('/login',methods=["POST"])
 def login():
     # Recieving details of the camp logging in
     email=request.form["Email"]
     password=request.form["password"]
 
-    if email not in db and email!="":
-        return render_template('login.html',info="Invalid username")
-    elif db[email]!=password and password!="":
-        return render_template('login.html',info="Invalid password")
-    elif db[email]==password:
-        return render_template('home.html')
+    # Checking if the user exists
+    user = User.query.filter_by(Email=email).first()
+    if user is None:
+        return flash("Invalid user")
+
+    # Checking if the password matches
+    elif not bcrypt.check_password_hash(user.password,password):
+        return flash("Invalid password")
+    
     else:
-        return
+        return render_template('home.html')
     
     
 
@@ -91,4 +148,4 @@ def create_user():
     return render_template('registration.html')
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
