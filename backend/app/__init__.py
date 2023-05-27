945d39f4ce9aa4077ec9c67fbff064e2be0003e9
Sayed Afnan Khazi <afnan@Sayeds-MacBook-Pro.local>
2023-05-27 13:17:52 +0530

    Models ready

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,8 +14,9 @@ def create_app(config_class = Config):
 
     # Creating the database tables (if not present)
 
-    # from app.models.refugee import Refugee
-    # from app.models.camp import Camp
+    from app.models.user import User
+    from app.models.budgets import Budgets
+    from app.models.transactions import Transactions
     
     with app.app_context():
         db.create_all()
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
 from app.extensions import db
+from sqlalchemy.orm import relationship
 
 @dataclass
 class User(db.Model):
-    
+    UserID: int = db.Column(db.Integer, primary_key=True)
+    FirstName: str = db.Column(db.String(50), nullable=False)
+    LastName: str = db.Column(db.String(50), nullable=False)
+    Email: str = db.Column(db.String(50), nullable=False)
+    Password: str = db.Column(db.String(50), nullable=False)
+    Balance: float = db.Column(db.Float, nullable=False)
+
+    # On Delete Cascade
+    transactions = relationship("Transactions", back_populates="User",cascade="all, delete-orphan") 
+
     def __repr__(self):
          return f'<User>'
